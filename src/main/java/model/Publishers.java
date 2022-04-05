147514c0c318fe116336dcbb98faa9a6e70b088c
Breanna <65743665+breanna-00@users.noreply.github.com>
2022-04-04 21:03:12 -0700

    Publishers Class

---
+++ src/main/java/model/Publishers.java
@@ -0,0 +1,52 @@
+package csulb.cecs323.model;
+/** From Josiah's branch.
+
+/*create table PUBLISHERS
+        (
+        NAME VARCHAR(80) not null
+        primary key,
+        EMAIL VARCHAR(80) not null
+        unique,
+        PHONE VARCHAR(24) not null
+        unique
+        );*/
+
+import javax.persistence.*;
+@Entity
+public class Publishers {
+    @Id
+    @Column(length = 80, nullable = false)
+    private String NAME;
+
+    @Column(length = 80, nullable = false, unique = true)
+    private String EMAIL;
+
+    @Column(length = 24, nullable = false, unique = true)
+    private String PHONE;
+
+    public Publishers (String NAME, String EMAIL, String PHONE){
+        this.setNAME(NAME);
+        this.setEMAIL(EMAIL);
+        this.setPHONE(PHONE);
+    }
+
+    public Publishers () {}
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public void setEMAIL(String email) {
+        this.EMAIL = email;
+    }
+
+    public void setPHONE(String PHONE) {
+        this.PHONE = PHONE;
+    }
+
+    @Override
+    public String toString () {
+        return "Publisher - Name: " + this.NAME + " Email: " + this.EMAIL +
+                " Phone: " + this.PHONE;
+    }
+}
