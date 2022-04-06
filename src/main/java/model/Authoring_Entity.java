8038618fa6e544385832f0c464147858d67bab49
Breanna <65743665+breanna-00@users.noreply.github.com>
2022-04-05 23:03:54 -0700

    Update Authoring_Entity.java

--- src/main/java/model/Authoring_Entity.java
+++ src/main/java/model/Authoring_Entity.java
@@ -19,8 +19,7 @@ public class Authoring_Entity {
     @Column (length=31)
     private String authoring_entity_type;
 
-    /*@OneToOne(fetch = FetchType.LAZY, mappedBy = "AUTHORING_ENTITY_NAME")
-    @Column (length=80, nullable = false)*/
+    @Column (length=80, nullable = false)
     private String name;
 
     @Column (length=80)
@@ -28,4 +27,56 @@ public class Authoring_Entity {
 
     @Column
     private int year_formed;
-}
+
+    public Authoring_Entity (String email, String authoring_entity_type, String name, String head_writer, int year_formed) {
+        this.setEmail(email);
+        this.setAuthoring_Entity_Type(authoring_entity_type);
+        this.setName(name);
+        this.setHead_Writer(head_writer);
+        this.setYear_Formed(year_formed);
+    }
+
+    public Authoring_Entity () {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String e) { this.email = e; }
+
+    public String getAuthoring_Entity_Type() {
+        return authoring_entity_type;
+    }
+
+    public void setAuthoring_Entity_Type(String a) { this.authoring_entity_type = a; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getHead_Writer() {
+        return head_writer;
+    }
+
+    public void setHead_Writer(String h) {
+        this.head_writer = h;
+    }
+
+    public int getYear_Formed() { return year_formed; }
+
+    public void setYear_Formed(int y) {
+        this.year_formed = y;
+    }
+
+    @Override
+    public String toString () {
+        return "Authoring_Entity - Email: " + this.getEmail() + " Type: " + this.getAuthoring_Entity_Type() +
+                " Name: " + this.getName() +
+                " Head Writer: " + this.getHead_Writer() +
+                " Year Formed: " + this.getYear_Formed();
+    }
+)
