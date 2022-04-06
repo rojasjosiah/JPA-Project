c39d9f1bd67eeeb79a4273711aec1f5a3c573c8c
Breanna <65743665+breanna-00@users.noreply.github.com>
2022-04-06 00:31:26 -0700

    Create Ad_Hoc_Team.java

---
+++ src/main/java/model/Ad_Hoc_Team.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Ad_Hoc_Team  extends Authoring_Entity{
+
+    public Ad_Hoc_Team (String email, String authoring_entity_type, String name){
+        this.setEmail(email);
+        this.setAuthoring_Entity_Type(authoring_entity_type);
+        this.setName(name);
+    }
+    public Ad_Hoc_Team() {}
+}
